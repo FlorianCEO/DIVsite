e3554229b39f22875a83fc9bb8b43af2779292f4
Florian CECCHELLERO <floriancecchellero@macbook-pro-de-florian.home>
2016-10-25 19:47:59 +0200

    Upload images via Paperclip to S3 AWS

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,9 +1,7 @@
 class Article < ActiveRecord::Base
   before_save :set_slug
-  has_attached_file :image, styles: { medium: "800x800>", thumb: "300x300>", default_url: "/images/default.jpg" },
-    :storage => :s3,
-    :s3_credentials => "#{Rails.root}/config/aws.yml",
-    :bucket => "divsite"
+  has_attached_file :image, styles: { medium: "800x800>", thumb: "300x300>", default_url: "/images/default.jpg" }
+
 
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
--- app/models/projet.rb
+++ app/models/projet.rb
@@ -1,8 +1,5 @@
 class Projet < ActiveRecord::Base
-  has_attached_file :image, styles: { medium: "800x800>", thumb: "300x300>" },
-    :storage => :s3,
-    :s3_credentials => "#{Rails.root}/config/aws.yml",
-    :bucket => "divsite"
+  has_attached_file :image, styles: { medium: "800x800>", thumb: "300x300>" }
 
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
