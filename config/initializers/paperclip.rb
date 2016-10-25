2fa1b44a2441129a97562cdd533e3a24c14d97ea
Florian CECCHELLERO <floriancecchellero@macbook-pro-de-florian.home>
2016-10-25 19:26:35 +0200

    Upload images via Paperclip to S3 AWS

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -4,6 +4,7 @@ Rails.application.config.before_initialize do
   Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
 end
 
-Paperclip::Attachment.default_options[:s3_host_name] = 'eu-standard.amazonaws.com'
+Paperclip::Attachment.default_options[:s3_host_name] = 's3-eu-west-1.amazonaws.com'
 Paperclip::Attachment.default_options[:s3_region] = 'eu-west-1'
 Paperclip::Attachment.default_options[:bucket] = 'divsite'
+
