d8c651c7aedce74a2d323008340249a41a580f6f
Florian CECCHELLERO <floriancecchellero@macbook-pro-de-florian.home>
2016-10-25 16:27:05 +0200

    Upload images via Paperclip to S3 AWS

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,17 +78,15 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # config paperclip to use S3 AWS
-  config.paperclip.defaults = {
-    storage: :s3,
-    s3_host_name: 's3-eu-west-1.amazonaws.com',
-    s3_credentials: {
-      access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
-      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-      s3_region: ENV.fetch('AWS_REGION'),
-      bucket: ENV.fetch('S3_BUCKET_NAME'),
-    },
+  config.paperclip_defaults = {
+  storage: :s3,
+  s3_credentials: {
+    bucket: ENV.fetch('S3_BUCKET_NAME'),
+    access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+    secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+    s3_region: ENV.fetch('AWS_REGION'),
+  }
+  :url =>':s3_domain_url',
+  :path => '/:class/:attachment/:id_partition/:style/:filename',
 }
 end
-
-
-
