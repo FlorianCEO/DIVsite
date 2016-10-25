1d01fbfb200c8aa9fdfd889fa54bf4075f45e128
Florian CECCHELLERO <floriancecchellero@macbook-pro-de-florian.home>
2016-10-25 16:24:52 +0200

    Upload images via Paperclip to S3 AWS

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -48,8 +48,8 @@ Rails.application.configure do
         access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
         secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
         s3_region: ENV.fetch('AWS_REGION'),
+        bucket: ENV.fetch('S3_BUCKET_NAME'),
       },
-      bucket: ENV.fetch('S3_BUCKET_NAME'),
   }
 
   #config.paperclip_defaults = {
